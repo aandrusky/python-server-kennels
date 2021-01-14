7687d3fe800da3ec7938e99f8bcfa9f3ed6c382e
Alan Andrusky <alanandrusky@gmail.com>
2021-01-14 10:32:38 -0600

    done with ch. 6

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
-from animals import get_all_animals, get_single_animal, create_animal
-from customers import get_all_customers, get_single_customer, create_customer
-from employees import get_all_employees, get_single_employee, create_employee
-from locations import get_all_locations, get_single_location, create_location
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
+from customers import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
+from employees import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee
+from locations import get_all_locations, get_single_location, create_location, delete_location, update_location
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
@@ -103,52 +103,59 @@ class HandleRequests(BaseHTTPRequestHandler):
         (resource, id) = self.parse_url(self.path)
 
         # Initialize new animal
-        new_animal = None
+        new_resource = None
 
         # Add a new animal to the list. Don't worry about
-        # the orange squiggle, you'll define the create_animal
-        # function next.
         if resource == "animals":
-            new_animal = create_animal(post_body)
-
-        # Encode the new animal and send in response
-        self.wfile.write(f"{new_animal}".encode())
-
-        # Initialize new location
-        new_location = None
+            new_resource = create_animal(post_body)
 
         # Add a new location to the list. Don't worry about
-        # the orange squiggle, you'll define the create_location
-        # function next.
         if resource == "locations":
-            new_location = create_location(post_body)
-
-        # Encode the new location and send in response
-        self.wfile.write(f"{new_location}".encode())
-
-        # Initialize new customer
-        new_customer = None
+            new_resource = create_location(post_body)
 
         # Add a new customer to the list. Don't worry about
-        # the orange squiggle, you'll define the create_customer
-        # function next.
         if resource == "customers":
-            new_customer = create_customer(post_body)
-
-        # Encode the new customer and send in response
-        self.wfile.write(f"{new_customer}".encode())
-
-        # Initialize new employee
-        new_employee = None
+            new_resource = create_customer(post_body)
 
         # Add a new employee to the list. Don't worry about
-        # the orange squiggle, you'll define the create_employee
-        # function next.
         if resource == "employees":
-            new_employee = create_employee(post_body)
+            new_resource = create_employee(post_body)
 
         # Encode the new employee and send in response
-        self.wfile.write(f"{new_employee}".encode())
+        self.wfile.write(f"{new_resource}".encode())
+
+
+    def do_DELETE(self):
+        # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+
+        # Encode the new animal and send in response
+
+
+    # Delete a single customer from the list
+        if resource == "customers":
+            delete_customer(id)
+
+        # Encode the new customer and send in response
+
+    # Delete a single employees from the list
+        if resource == "employees":
+            delete_employee(id)
+
+        # Encode the new employee and send in response
+
+        if resource == "locations":
+            delete_location(id)
+
+        # Encode the new location and send in response
+        self.wfile.write("".encode())
 
 
 
@@ -156,6 +163,34 @@ class HandleRequests(BaseHTTPRequestHandler):
     # It handles any PUT request.
     def do_PUT(self):
         self.do_POST()
+
+    def do_PUT(self):
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+
+        # Delete a single customer from the list
+        if resource == "customers":
+            update_customer(id, post_body)
+
+        # Delete a single employee from the list
+        if resource == "employees":
+            update_employee(id, post_body)
+
+        # Delete a single location from the list
+        if resource == "locations":
+            update_location(id, post_body)
+
+        # Encode the new module and send in response
+        self.wfile.write("".encode())
 
 
 # This function is not inside the class. It is the starting
